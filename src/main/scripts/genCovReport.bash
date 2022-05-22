c24bc0f06178c03652728f76584437c52dc4984f
Rick Richmond <richardrichmond@fastmail.fm>
2022-05-22 23:20:41 +0100

    Adding check to verify HTML coverage generated OK

--- src/main/scripts/genCovReport.bash
+++ src/main/scripts/genCovReport.bash
@@ -10,3 +10,9 @@ mkdir -p coverage/html
 verilator_coverage --write-info coverage/coverage.info coverage/merged.dat
 lcov -a coverage/coverage.info -o coverage/lcov.info
 genhtml -o coverage/html coverage/lcov.info 
+if [ -f "coverage/html/index.html" ]; then
+  echo "Generated coverage/html/index.html"
+else
+  echo "Error: failed to generate coverage/html/index.html!"
+  exit
+fi
