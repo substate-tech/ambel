25ab6c992e2fabbb5c4a68d715818f88db345c15
Rick Richmond <richardrichmond@fastmail.fm>
2022-05-31 00:23:56 +0100

    Trying to fix issue merging coverage

--- src/main/scripts/mergeModuleCov.bash
+++ src/main/scripts/mergeModuleCov.bash
@@ -1,20 +1,15 @@
 #!/bin/bash
-# Chisel Module name must be passed on the command line
-if [ ! -f "src/main/scala/$1.scala" ]; then
-  echo "Error: No scala file found for Chisel Module $1!"
-  exit
-fi
 
 # First check for coverage data
-datfiles=$(find test_run_dir/$1* -name "coverage.dat")
+datfiles=$(find test_run_dir/* -name "coverage.dat")
 
 if [ -z "$datfiles" ]; then
-  echo "Error: No coverage.dat files found for Chisel Module $1!"
+  echo "Error: No coverage.dat files found!"
   exit
 fi
 
 # Find all the generated *.sv files
-svfiles=$(find test_run_dir -name "$1*.sv")
+svfiles=$(find test_run_dir/* -name "*.sv")
 
 # Copy them to top level, overwrite duplicates (they will be identical)
 for file in ${svfiles[@]}; do
@@ -23,4 +18,4 @@ done
 
 # Merge coverage 
 mkdir -p coverage
-verilator_coverage -write coverage/merged.dat test_run_dir/$1*/coverage.dat
+verilator_coverage -write coverage/merged.dat $datfiles
